3e293d2b156b03cacdbc10dbaea702cf070e0ddb
BillAng <kenshinasuka@gmail.com>
2022-12-15 22:57:53 +0800

    Add project files.

---
+++ Data/TestEFContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestEF.Models;
+
+namespace TestEF.Data
+{
+    public class TestEFContext : DbContext
+    {
+        public TestEFContext (DbContextOptions<TestEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestEF.Models.Player> Player { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestEF.Models.Player>().ToTable("Player");
+        }
+    }
+}
---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace TestEF.Models
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public int Points { get; set; }
+    }
+}
